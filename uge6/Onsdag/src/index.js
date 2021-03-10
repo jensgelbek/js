75056575ceb2f48cdcf9b9691c45e5b96d5c830a
jensgelbek <jensgelbek@gmail.com>
2021-03-10 14:45:24 +0100

    ex1+2 onsdag uge 6

--- uge6/Onsdag/src/index.js
+++ uge6/Onsdag/src/index.js
@@ -10,14 +10,52 @@ document.getElementById("all-content").style.display = "block"
 */
 
 /* JS For Exercise-1 below */
+function showAllJokes(){
+  let jokes=jokeFacade.getJokes().map(joke=>"<li>"+joke+"</li>").join("\n")
+  document.getElementById("jokes").innerHTML=jokes
+}
+showAllJokes()
 
 
+
+document.getElementById("formjoke").addEventListener("submit", function (event) {
+  event.preventDefault();
+  let id = document.getElementById("jokeid").value;
+ document.getElementById("ex1joke").innerHTML="<h1>"+jokeFacade.getJokeById(id)+"</h1>"
+});
+
+
+document.getElementById("formaddjoke").addEventListener("submit", function (event) {
+  event.preventDefault();
+  let joke= document.getElementById("joketxt").value;
+  document.getElementById("joketxt").value=""
+ jokeFacade.addJoke(joke);
+ showAllJokes();
+});
 /* JS For Exercise-2 below */
 
+function postChuckJoke(){
+  fetch('https://api.chucknorris.io/jokes/random')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+   document.getElementById("chuckjoke").innerHTML=data.value+"<br>url: "+data.url;
+ });
+ 
 
+ 
 
+}
+
+document.getElementById("chuck").addEventListener("click", function (event) {
+  event.preventDefault();
+  postChuckJoke();
+});
+
+setInterval(function(){postChuckJoke(); }, 6000);
 /* JS For Exercise-3 below */
-
+   
 
 /* 
 Do NOT focus on the code below, UNLESS you want to use this code for something different than
